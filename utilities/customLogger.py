691c2d4fd9825e25017dee666719be0e2b392cea
lukesh <adelukesh@gmail.com>
2022-08-03 08:18:58 +0530

    fixed

--- utilities/customLogger.py
+++ utilities/customLogger.py
@@ -5,8 +5,7 @@ class LogGen:
     @staticmethod
     def loggen():
         logging.basicConfig(filename='.\\Logs\\automation.log',
-                            format='%(asctime)s: %(levelname)s: %(massage)s', datefmt='%m/%d/%Y %I:%M:%S %p',
-                            level=logging.INFO)
+                            format='%(asctime)s: %(levelname)s: %(massage)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 
         logger = logging.getLogger()
         logger.setLevel(logging.INFO)
