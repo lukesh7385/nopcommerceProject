9b05dfeed5bf47c19a503b566056c751757a8102
lukesh7385 <83576819+lukesh7385@users.noreply.github.com>
2022-08-03 07:57:32 +0530

    Update customLogger.py

--- utilities/customLogger.py
+++ utilities/customLogger.py
@@ -5,7 +5,7 @@ class LogGen:
     @staticmethod
     def loggen():
         logging.basicConfig(filename='.\\Logs\\automation.log',
-                            format='%(asctime)s- %(levelname)s- %(massage)s', datefmt='%m/%d/%Y %I:%M:%S %p',
+                            format='%(asctime)s: %(levelname)s: %(massage)s', datefmt='%m/%d/%Y %I:%M:%S %p',
                             level=logging.info)
 
         logger = logging.getLogger()
