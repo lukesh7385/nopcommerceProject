402579dd2f16b7c420b4ee75c9e25407e4782170
lukesh <adelukesh@gmail.com>
2022-10-04 18:01:09 +0530

    fixed

--- utilities/customLogger.py
+++ utilities/customLogger.py
@@ -2,17 +2,6 @@ import inspect
 import logging
 
 
-# class LogGen:
-#     @staticmethod
-#     def loggen():
-#         logging.basicConfig(filename='.\\Logs\\automation.log',
-#                             format='%(asctime)s | %(levelname)9s | %(filename)s:%(lineno)d | %(message)s'
-#                             , datefmt='%m/%d/%Y %I:%M:%S %p')
-#
-#         logger = logging.getLogger()
-#         logger.setLevel(logging.INFO)
-#         return logger
-
 class LogGen:
     @staticmethod
     def loggen(loglevel=logging.DEBUG):
